2b33c53b9517fa5f7b8106e6c644d6ae76f05602
Arafath1010 <107343055+Arafath1010@users.noreply.github.com>
2023-05-29 15:28:11 +0530

    Update whatsapp_api.py

--- whatsapp_api.py
+++ whatsapp_api.py
@@ -6,7 +6,6 @@ import requests
 from PIL import Image
 import cv2
 import imutils
-from transform import perspective_transform
 import numpy as np
 
 
@@ -54,6 +53,35 @@ def chatgpt():
         #replace the url 
         msg.media("https://whatsapp-vz43.onrender.com/static/"+name)    
         return str(bot_resp)
+    
+    
+    
+    
+    
+    # scanner codes
+    def order_points(pts):
+        rect = np.zeros((4, 2), dtype = "float32")
+        s = pts.sum(axis = 1)
+        rect[0] = pts[np.argmin(s)]
+        rect[2] = pts[np.argmax(s)]
+        diff = np.diff(pts, axis = 1)
+        rect[1] = pts[np.argmin(diff)]
+        rect[3] = pts[np.argmax(diff)]
+        return rect
+
+    def perspective_transform(image, pts):
+        rect = order_points(pts)
+        (tl, tr, br, bl) = rect
+        widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+        widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+        maxWidth = max(int(widthA), int(widthB))
+        heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+        heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+        maxHeight = max(int(heightA), int(heightB))
+        dst = np.array([[0, 0],[maxWidth - 1, 0],[maxWidth - 1, maxHeight - 1],[0, maxHeight - 1]], dtype = "float32")
+        transform_matrix = cv2.getPerspectiveTransform(rect, dst)
+        warped = cv2.warpPerspective(image, transform_matrix, (maxWidth, maxHeight))
+        return warped
     
     if "scan" in incoming_que:
         print("scanning")
