aab98b7551c1893c6c3d9c6cbb6ca062f79ccc9a
Arafath1010 <107343055+Arafath1010@users.noreply.github.com>
2023-06-17 10:36:39 +0530

    Update whatsapp_api.py

--- whatsapp_api.py
+++ whatsapp_api.py
@@ -59,29 +59,7 @@ def chatgpt():
     
     
     # scanner codes
-    def order_points(pts):
-        rect = np.zeros((4, 2), dtype = "float32")
-        s = pts.sum(axis = 1)
-        rect[0] = pts[np.argmin(s)]
-        rect[2] = pts[np.argmax(s)]
-        diff = np.diff(pts, axis = 1)
-        rect[1] = pts[np.argmin(diff)]
-        rect[3] = pts[np.argmax(diff)]
-        return rect
 
-    def perspective_transform(image, pts):
-        rect = order_points(pts)
-        (tl, tr, br, bl) = rect
-        widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
-        widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
-        maxWidth = max(int(widthA), int(widthB))
-        heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
-        heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
-        maxHeight = max(int(heightA), int(heightB))
-        dst = np.array([[0, 0],[maxWidth - 1, 0],[maxWidth - 1, maxHeight - 1],[0, maxHeight - 1]], dtype = "float32")
-        transform_matrix = cv2.getPerspectiveTransform(rect, dst)
-        warped = cv2.warpPerspective(image, transform_matrix, (maxWidth, maxHeight))
-        return warped
     
     if "scan" in incoming_que:
         print("scanning")
@@ -90,59 +68,140 @@ def chatgpt():
         if img.mode == 'RGBA':
                img = image.convert('RGB')
 
-        open_cv_image = np.array(img)
-        # Passing the image path
-        original_img = open_cv_image
-        copy = original_img.copy()
-
-        # The resized height in hundreds
-        ratio = original_img.shape[0] / 500.0
-        img_resize = imutils.resize(original_img, height=500)
-
-        gray_image = cv2.cvtColor(img_resize, cv2.COLOR_BGR2GRAY)
-
-        blurred_image = cv2.GaussianBlur(gray_image, (5, 5), 0)
-        edged_img = cv2.Canny(blurred_image, 75, 200)
-
-        cnts, _ = cv2.findContours(edged_img, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-        cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:5]
-        try:
-            for c in cnts:
-                peri = cv2.arcLength(c, True)
-                approx = cv2.approxPolyDP(c, 0.02 * peri, True)
-                if len(approx) == 4:
-                    doc = approx
-                    break
-
-            p = []
-
-            for d in doc:
-                tuple_point = tuple(d[0])
-                cv2.circle(img_resize, tuple_point, 3, (0, 0, 255), 4)
-                p.append(tuple_point)
-
-            warped_image = perspective_transform(copy, doc.reshape(4, 2) * ratio)
-            warped_image = cv2.cvtColor(warped_image,cv2.COLOR_BGR2RGB)
-
-            #T = threshold_local(warped_image,21, offset=10, method="gaussian")
-            #warped = (warped_image > T).astype("uint8") * 255
-            cv2.imwrite("static/"+name,warped_image)
-            print("https://whatsapp-vz43.onrender.com/static/"+name)
-            bot_resp = MessagingResponse()
-            msg = bot_resp.message()
-            #replace the url 
-            msg.media("https://whatsapp-vz43.onrender.com/static/"+name)    
-            return str(bot_resp)
-            #return "http://127.0.0.1:8000/static/output/scan-"+name
-        except:
-            print("image have no borders so try to upload image with borders")
-            bot_resp = MessagingResponse()
-            msg = bot_resp.message()
-            #replace the url 
-            msg.body("image have no borders so try to upload image with borders")    
-            return str(bot_resp)
+        def order_points(pts):
+            """Rearrange coordinates to order:
+               top-left, top-right, bottom-right, bottom-left"""
+            rect = np.zeros((4, 2), dtype='float32')
+            pts = np.array(pts)
+            s = pts.sum(axis=1)
+            # Top-left point will have the smallest sum.
+            rect[0] = pts[np.argmin(s)]
+            # Bottom-right point will have the largest sum.
+            rect[2] = pts[np.argmax(s)]
+        
+            diff = np.diff(pts, axis=1)
+            # Top-right point will have the smallest difference.
+            rect[1] = pts[np.argmin(diff)]
+            # Bottom-left will have the largest difference.
+            rect[3] = pts[np.argmax(diff)]
+            # return the ordered coordinates
+            return rect.astype('int').tolist()
     
+    
+        def scan(img):
+            # Resize image to workable size
+            dim_limit = 1080
+            max_dim = max(img.shape)
+            if max_dim > dim_limit:
+                resize_scale = dim_limit / max_dim
+                img = cv2.resize(img, None, fx=resize_scale, fy=resize_scale)
+        
+            # Create a copy of resized original image for later use
+            orig_img = img.copy()
+            # cv2.imshow("original_resized", orig_img)
+        
+            # Repeated Closing operation to remove text from the document.
+            kernel = np.ones((5, 5), np.uint8)
+            img = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel, iterations=3)
+            # cv2.imshow("morphologyEX", img)
+        
+            # GrabCut
+            mask = np.zeros(img.shape[:2], np.uint8)
+            bgdModel = np.zeros((1, 65), np.float64)
+            fgdModel = np.zeros((1, 65), np.float64)
+            rect = (20, 20, img.shape[1] - 20, img.shape[0] - 20)
+            cv2.grabCut(img, mask, rect, bgdModel, fgdModel, 5, cv2.GC_INIT_WITH_RECT)
+            mask2 = np.where((mask == 2) | (mask == 0), 0, 1).astype('uint8')
+            img = img * mask2[:, :, np.newaxis]
+            # cv2.imshow("grabcut", img)
+        
+            # Convert to grayscale.
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            gray = cv2.GaussianBlur(gray, (11, 11), 0)
+            # cv2.imshow("gray_blurred", gray)
+        
+            # Edge Detection.
+            canny = cv2.Canny(gray, 0, 200)
+            canny = cv2.dilate(canny, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5)))
+            # cv2.imshow("canny_dilate", canny)
+        
+            # Finding contours for the detected edges.
+            contours, hierarchy = cv2.findContours(canny, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+            # Keeping only the largest detected contour.
+            page = sorted(contours, key=cv2.contourArea, reverse=True)[:5]
+        
+            # Detecting Edges through Contour approximation.
+            # Loop over the contours.
+            if len(page) == 0:
+                return orig_img
+            for c in page:
+                # Approximate the contour.
+                epsilon = 0.02 * cv2.arcLength(c, True)
+                corners = cv2.approxPolyDP(c, epsilon, True)
+                # If our approximated contour has four points.
+                if len(corners) == 4:
+                    break
+        
+            # Sorting the corners and converting them to desired shape.
+            corners = sorted(np.concatenate(corners).tolist())
+            # For 4 corner points being detected.
+            # Rearranging the order of the corner points.
+            corners = order_points(corners)
+            (tl, tr, br, bl) = corners
+        
+            # Draw points
+            points_img = cv2.cvtColor(canny, cv2.COLOR_GRAY2BGR)
+            point_count = 0
+            for corner in corners:
+                cv2.circle(points_img, corner, 3, (255, 0, 0), -1)
+                point_count += 1
+                cv2.putText(points_img, str(point_count), corner, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            # cv2.imshow("points", points_img)
+        
+            # Finding the maximum width.
+            widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+            widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+            maxWidth = max(int(widthA), int(widthB))
+        
+            # Finding the maximum height.
+            heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+            heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+            maxHeight = max(int(heightA), int(heightB))
+        
+            # Final destination co-ordinates.
+            destination_corners = [[0, 0], [maxWidth, 0], [maxWidth, maxHeight], [0, maxHeight]]
+        
+            # Getting the homography.
+            M = cv2.getPerspectiveTransform(np.float32(corners), np.float32(destination_corners))
+            # Perspective transform using homography.
+            final = cv2.warpPerspective(orig_img, M, (maxWidth, maxHeight), flags=cv2.INTER_LINEAR)
+        
+            return final
 
+
+        #runtime = []
+        for img_path in ["image"]:
+            try:
+                #img = cv2.imread("kk.jpeg")
+                print(img_path)
+        
+                t1 = time.time()
+        
+                scanned_img = scan(img)
+        
+                t2 = time.time()
+        
+                #runtime.append({'image': img_path, 'time': t2 - t1})
+        
+                #cv2.imshow("scanner", scanned_img)
+                cv2.imwrite('static/' + name, scanned_img)
+                print("scanned"+name)
+        
+                key = cv2.waitKey(0)
+                if key == 27:
+                    break
+            except:
+                print('fail')
 
 
 if __name__ == '__main__':
