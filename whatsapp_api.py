ee093d99da74a95fb66d8ea4827c2ddb51d0c60a
Arafath1010 <107343055+Arafath1010@users.noreply.github.com>
2023-06-17 13:20:05 +0530

    Update whatsapp_api.py

--- whatsapp_api.py
+++ whatsapp_api.py
@@ -48,39 +48,6 @@ def chat(From: str = Form(...),MediaUrl0:str = Form(...), Body: str = Form(...))
         return Response(content=str(response), media_type="application/xml")
 
     
-    incoming_que = request.values.get('Body', '').lower()
-    print("Question: ", incoming_que)
-    link = request.values.get('MediaUrl0')
-    print(link)
-    if link is not None:
-       li.append(link)
-
-    if "to pdf" in incoming_que:
-        print("converting")
-        images=[]
-        for url in li:
-            image = Image.open(requests.get(url, stream=True).raw)
-
-            if image.mode == 'RGBA':
-               image = image.convert('RGB')
-            images.append(image)
-        
-        
-        
-        name = url.split("/")[-1]+".pdf"
-        if len(li)==1:
-           images[0].save("static/"+name, "PDF" ,resolution=100.0, save_all=True)
-        else:
-           images[0].save("static/"+name, "PDF" ,resolution=100.0, save_all=True,append_images=images[1:])
-        li=[]
-        print("https://whatsapp-vz43.onrender.com/static/"+name)
-        bot_resp = MessagingResponse()
-        msg = bot_resp.message()
-        #replace the url 
-        msg.media("https://whatsapp-vz43.onrender.com/static/"+name)    
-        return str(bot_resp)
-    
-    
     
     
     
