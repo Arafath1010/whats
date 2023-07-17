0e2c5772d59794aa0ff39734ac0a015a60e4815e
Arafath1010 <107343055+Arafath1010@users.noreply.github.com>
2023-07-17 23:46:44 +0530

    Update whatsapp_api.py

--- whatsapp_api.py
+++ whatsapp_api.py
@@ -90,7 +90,7 @@ async def chat(From: str = Form(...),MediaUrl0:str = Form(None), Body: str = For
             file.close()
             print("file saved",MediaUrl0)
             response = MessagingResponse() 
-            msg = response.message(): 
+            msg = response.message()
             msg.body(data['answer'])
             msg.body("please ask question to anlyze your data !")
             return Response(content=str(response), media_type="application/xml")
